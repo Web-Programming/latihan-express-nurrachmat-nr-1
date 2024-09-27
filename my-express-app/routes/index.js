36fe6c8a22e3a73d9145fbaa35feb0c969c0eeb3
nur rachmat <rachmat.nur91@gmail.com>
2024-09-27 18:27:45 +0700

    Express App
    Latihan membuat route dan view

---
+++ my-express-app/routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get("/about", function(req, res, next) {
+  res.render('about', { title: 'About Us' });
+});
+
+router.get("/contact", function(req, res, next) {
+  res.render('contact', { title: 'Contact Us' });
+});
+
+// Route to handle form submission
+router.post('/submit-contact', (req, res) => {
+    const { name, email, message } = req.body;
+    // Handle the form data (e.g., save to database, send an email, etc.)
+    console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+    
+    // Redirect or send a response
+    res.render('thank-you', {title: 'Thank You', name : name}); 
+});
+
+
+module.exports = router;
