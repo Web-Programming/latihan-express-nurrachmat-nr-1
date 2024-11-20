f6919379498c5efc1348a2136390518c0e316073
Nur Rachmat <rachmat.nur91@gmail.com>
2024-11-20 12:39:24 +0000

    Form

--- belajar_angular/src/app/details/details.component.ts
+++ belajar_angular/src/app/details/details.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
+import { FormGroup, FormControl, ReactiveFormsModule  } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
      <article >
       <img class="listing-photo" [src]="housingLocation?.photo"
@@ -24,6 +25,17 @@ import { HousingLocation } from '../housing-location';
           <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
         </ul>
       </section>
+      <section class="listing-apply">
+        <h1 class="section-heading">Apply to live here</h1>
+        <form [formGroup]="applyForm" (submit)="submitApplyForm()">
+          <label for="first-name">First Name</label>
+          <input type="text" id="first-name" formControlName="firstName" placeholder="Input first name">
+          <label for="last-name">Last Name</label>
+          <input type="text" id="last-name" formControlName="lastName" placeholder="Input last name">
+          <button type="submit" class="primary">Apply</button>
+        </form>
+      </section>
+    </article>
   `,
   styleUrl: './details.component.css'
 })
@@ -32,10 +44,22 @@ export class DetailsComponent {
   housingLocationId = 0;
   housingService: HousingService = inject(HousingService);
   housingLocation: HousingLocation | undefined
+  applyForm: FormGroup = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl('')
+  })
 
   constructor(){
     this.housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId)
     console.table(this.housingLocation)
+  }
+
+  submitApplyForm(){
+    //alert("Hallo you submit a form");
+    //alert("Hallo : " + this.applyForm.value.firstName 
+    //  + " " + this.applyForm.value.lastName)
+
+    //panggil API simpan data registarsi via service
   }
 }
