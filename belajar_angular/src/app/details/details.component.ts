c6e6c73460d4e10ea665c18d84c9fb8ccaa21578
Nur Rachmat <rachmat.nur91@gmail.com>
2024-11-22 17:56:31 +0700

    Get API

--- belajar_angular/src/app/details/details.component.ts
+++ belajar_angular/src/app/details/details.component.ts
@@ -55,7 +55,10 @@ export class DetailsComponent {
 
   constructor(){
     this.housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId)
+    this.housingService.getHousingLocationById(this.housingLocationId)
+      .then(location => {
+        this.housingLocation = location;
+      })
     console.table(this.housingLocation)
   }
 
